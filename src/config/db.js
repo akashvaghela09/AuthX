80d7ade32fce9f54d1823825ca9f8c214e34ae8e
akashvaghela09 <akashvaghela09@gmail.com>
2021-06-30 23:35:40 +0530

    file updated for deprecated values

--- src/config/db.js
+++ src/config/db.js
@@ -5,7 +5,8 @@ const connect = async () => {
     return new mongoose.connect(process.env.DATABASE_URL, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
-        useCreateIndex: true
+        useCreateIndex: true,
+        useFindAndModify: false
     })
 }
 
