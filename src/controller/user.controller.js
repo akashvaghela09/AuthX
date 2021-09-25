b0033b5b0a99c78d31a82e23b2149314fb99289b
akashvaghela09 <akashvaghela09@gmail.com>
2021-09-26 00:41:34 +0530

    bcrypt changed to bcryptjs

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const router = express.Router()
 const {UserUpload} = require("../model/user.model")
-const bcrypt = require("bcrypt")
+const bcrypt = require("bcryptjs")
 
 // Get User
 router.get("/", async (req, res) => {
